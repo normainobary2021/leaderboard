1b9a60d0c2b1486b6ec61055a91af51858a02b7b
normainobary2021 <norman.ainobushoborozi@gmail.com>
2022-09-11 06:59:46 +0300

    Make linter changes

--- src/modules/consumeAPIModule.js
+++ src/modules/consumeAPIModule.js
@@ -1,19 +1,19 @@
 const fetchScores = async (actual, id) => {
-    const response = await fetch(`${actual}${id}`)
-      .then((response) => response.json());
-    return response.result;
-  };
-  
-  const printScore = async (actual, id, data) => {
-    const response = await fetch(`${actual}${id}`, {
-      method: 'POST',
-      body: data,
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-      .then((response) => response.json());
-    return response;
-  };
-  
-  module.exports = { fetchScores, printScore };
+  const response = await fetch(`${actual}${id}`)
+    .then((response) => response.json());
+  return response.result;
+};
+
+const printScore = async (actual, id, data) => {
+  const response = await fetch(`${actual}${id}`, {
+    method: 'POST',
+    body: data,
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json());
+  return response;
+};
+
+module.exports = { fetchScores, printScore };
